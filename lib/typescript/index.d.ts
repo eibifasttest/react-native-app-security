9ac7b29d0410bbe9cc97b979da2931a7b4aab7e5
chonglim <chonglim.tan@ifastfinancial.com>
2024-01-22 17:56:44 +0800

    remove bob script

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,6 @@
+export interface AppSecurityType {
+    isDeviceRooted(): Promise<boolean>;
+    isDebugEnabled(): Promise<boolean>;
+}
+export declare const AppSecurity: AppSecurityType;
+//# sourceMappingURL=index.d.ts.map
