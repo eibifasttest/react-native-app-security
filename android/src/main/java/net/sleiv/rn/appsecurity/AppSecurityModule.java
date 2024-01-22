44f00535a4d75a4e379a2ec63f5b23ee1c8b9ce4
chonglim <chonglim.tan@ifastfinancial.com>
2024-01-22 11:09:50 +0800

    refactor from kotlin to java

---
+++ android/src/main/java/net/sleiv/rn/appsecurity/AppSecurityModule.java
@@ -0,0 +1,40 @@
+package net.sleiv.rn.appsecurity;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = AppSecurityModule.NAME)
+public class AppSecurityModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "AppSecurity";
+
+  public AppSecurityModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  @ReactMethod
+  public void isDeviceRooted(final Promise promise) {
+    promise.resolve(true);
+  }
+
+  @ReactMethod
+  public void isDebugEnabled(final Promise promise) {
+    promise.resolve(true);
+  }
+
+  @ReactMethod
+  public void isIncorrectFingerprint(final Promise promise) {
+    promise.resolve(true);
+  }
+
+}
