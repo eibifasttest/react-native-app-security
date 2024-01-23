1f101365fa31cbe0163c2db06283e86e51a8fb98
chonglim <chonglim.tan@ifastfinancial.com>
2024-01-23 09:29:12 +0800

    Revert "remove bob script"
    This reverts commit 9ac7b29d0410bbe9cc97b979da2931a7b4aab7e5.

--- lib/typescript/index.d.ts
+++
@@ -1,6 +0,0 @@
-export interface AppSecurityType {
-    isDeviceRooted(): Promise<boolean>;
-    isDebugEnabled(): Promise<boolean>;
-}
-export declare const AppSecurity: AppSecurityType;
-//# sourceMappingURL=index.d.ts.map
